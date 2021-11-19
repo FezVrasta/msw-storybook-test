0c492b14d19acaa0463645b8bb01ac0a234ed4fe
Federico Zivolo <federico.zivolo@gmail.com>
2021-11-19 16:28:02 +0100

    storyshots + mws broken repro

---
+++ src/storybook.test.js
@@ -0,0 +1,8 @@
+import initStoryshots from '@storybook/addon-storyshots';
+
+// The required import from the @storybook/addon-storyshots-puppeteer addon
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+
+initStoryshots({
+  test: imageSnapshot(),
+});
